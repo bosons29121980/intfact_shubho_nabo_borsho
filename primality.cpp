06bc597194826946dc72f01562c8e73b2d8b0593
Subhendra Basu <basu.subhendra@outlook.com>
2023-04-09 11:01:32 +0530

    code

--- primality.cpp
+++ primality.cpp
@@ -1,6 +1,5 @@
 #ifndef __PRIMALITY__
 #define __PRIMALITY__
-#include <string>
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
@@ -16,10 +15,7 @@ int is_prime(char* num) {
 	fread(buf, 2, sizeof(char), fp);
 	fread(buf, 2, sizeof(char), fe);
 	free(buf);
-	int ret = 0, last_index = 0, index = 0;
 	unsigned long long int ctr = 0;
-	unsigned long long int ptr = 0, last_ptr = 0;
-	std::string num_string = num;
 	while (1) {
 		char pp = 0, ee = 0;
 		fscanf(fp, "%c", &pp);
@@ -32,29 +28,13 @@ int is_prime(char* num) {
 		buf[3] = '\0';
 		int nk = atoi(buf);
 		if (nk % 8 == 0) {
-			last_ptr = ptr;
-			ptr = ctr + 1;
-			unsigned long long int delta = ptr - last_ptr;
-			printf("Delta %llu\n", delta);
-			system("a=1;read a");
-			std::string delta_string = boost::lexical_cast<std::string>(delta);
-			index = num_string.find(delta_string, last_index);
-			int delta_index = index - last_index;
-			if (delta_index == delta_string.size()) {
-				last_index = index;
-				if (last_index == num_string.size() - 1) {
-					ret = 0;
-					break;
-				}
-			} else
-				ret = 1;
-			break;
+printf("%d\n", nk);
+system("a=1;read a");
 		}
 		++ctr;
 	}
 	fclose(fp);
 	fclose(fe);
-	return ret;
 }
 
 int main(int argc, char* argv[]) {
